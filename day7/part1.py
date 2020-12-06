4fba7cd154bfb6a61509e3fe8623bed41fe174b1
dev <keep.it.simple.dev@gmail.com>
2020-12-06 14:58:11 -0500

    add day 7 skeleton

---
+++ day7/part1.py
@@ -0,0 +1,14 @@
+import unittest
+
+from aoc_util import assert_with_message
+from aoc_util import get_input
+
+
+def run():
+    print()
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_today(self):
+        run()
