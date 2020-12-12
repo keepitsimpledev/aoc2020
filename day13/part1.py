c1053236eebc15cd21ad50475e4b2c056e7600c4
dev <keep.it.simple.dev@gmail.com>
2020-12-12 01:19:14 -0500

    day13 skeleton

---
+++ day13/part1.py
@@ -0,0 +1,15 @@
+import unittest
+import copy
+
+from aoc_util import assert_with_message
+from aoc_util import get_input
+
+
+def run():
+    print()
+
+
+class TestAoC(unittest.TestCase):
+
+    def test_part(self):
+        run()
