361a0cff0bb1a222eebf62d549098245e56629fc
dev <keep.it.simple.dev@gmail.com>
2020-12-13 23:30:08 -0500

    day13 part2 another improvement effort - another insufficient solution

--- day13/part2.py
+++ day13/part2.py
@@ -15,67 +15,41 @@ def process_input(time_and_schedules):
 
 
 def find_consecutive_schedule(busses_and_intervals):
-    current_values = []
-    for item in busses_and_intervals:
-        current_values += [item[0]]
-    highest_and_second = find_highest_and_penultimate(busses_and_intervals)
+    busses_and_intervals = sorted(busses_and_intervals, reverse=True, key=lambda tup: tup[0])
+    solution = []
     for i in range(len(busses_and_intervals)):
-        if busses_and_intervals[i][0] == highest_and_second[0][0]:
-            fulcrum_index = i
-            break
-    current_values[fulcrum_index] = get_consecutive_schedule_with_minimum(highest_and_second[0],
-                                                                          highest_and_second[1], 0)
+        solution += [busses_and_intervals[i][0]]
+        if busses_and_intervals[i][1] == 0:
+            solution_index = i
+
     solution_found = False
     while not solution_found:
-        current_solution_looks_good = True
-        left_index = fulcrum_index
-        while left_index > 0:
-            current_value = current_values[left_index]
-            left_bus = busses_and_intervals[left_index - 1][0]
-            left_gap = busses_and_intervals[left_index][1] - busses_and_intervals[left_index - 1][1]
-            if (current_value - left_gap) % left_bus == 0:
-                current_values[left_index - 1] = current_value - left_gap
-                left_index -= 1
-            else:
-                current_solution_looks_good = False
-                break
-        if current_solution_looks_good:
-            right_index = fulcrum_index
-            while right_index < len(busses_and_intervals) - 1:
-                current_value = current_values[right_index]
-                right_bus = busses_and_intervals[right_index + 1][0]
-                right_gap = busses_and_intervals[right_index + 1][1] - busses_and_intervals[right_index][1]
-                if (current_value + right_gap) % right_bus == 0:
-                    current_values[right_index + 1] = current_value + right_gap
-                    right_index += 1
-                else:
-                    current_solution_looks_good = False
+        current_solution_is_good = True
+        for i in range(len(busses_and_intervals) - 1):
+            gap_forward = busses_and_intervals[i + 1][1] - busses_and_intervals[i][1]
+            schedule = get_consecutive_schedule_with_minimum(busses_and_intervals[i],
+                                                             busses_and_intervals[i + 1],
+                                                             [solution[i], solution[i] + gap_forward])
+            solution[i] = schedule[0]
+            solution[i + 1] = schedule[1]
+            if i > 0:
+                gap_backward = busses_and_intervals[i][1] - busses_and_intervals[i - 1][1]
+                if solution[i] - gap_backward != solution[i - 1]:
+                    current_solution_is_good = False
                     break
-        if current_solution_looks_good:
-            return current_values[0]
+        if not current_solution_is_good:
+            solution[0] += busses_and_intervals[0][0]
         else:
-            current_values[fulcrum_index] = get_consecutive_schedule_with_minimum(
-                highest_and_second[0], highest_and_second[1], current_values[fulcrum_index] + 1)
+            solution_found = True
+    return solution[solution_index]
 
 
-def find_highest_and_penultimate(bus_and_interval):
-    highest = [-1, -1]
-    second_highest = [-1, -1]
-    for item in bus_and_interval:
-        if item[0] > highest[0]:
-            second_highest = highest
-            highest = item
-        elif second_highest[0] < item[0] <= highest[0]:
-            second_highest = item
-    return [highest, second_highest]
-
-
-def get_consecutive_schedule_with_minimum(bus1, bus2, minimum):
-    bus1_time = int(minimum / bus1[0]) * bus1[0]
-    if bus1_time < minimum or bus1_time == 0:
+def get_consecutive_schedule_with_minimum(bus1, bus2, minima):
+    bus1_time = int(minima[0] / bus1[0]) * bus1[0]
+    if bus1_time < minima[0] or bus1_time == 0:
         bus1_time += bus1[0]
-    bus2_time = int(minimum / bus2[0]) * bus2[0]
-    if bus2_time < minimum or bus2_time == 0:
+    bus2_time = int(minima[1] / bus2[0]) * bus2[0]
+    if bus2_time < minima[1] or bus2_time == 0:
         bus2_time += bus2[0]
     gap = bus2[1] - bus1[1]
     while bus1_time != bus2_time - gap:
@@ -83,7 +57,7 @@ def get_consecutive_schedule_with_minimum(bus1, bus2, minimum):
             bus1_time += bus1[0]
         else:
             bus2_time += bus2[0]
-    return bus1_time
+    return [bus1_time, bus2_time]
 
 
 def run():
@@ -93,15 +67,10 @@ def run():
 class TestAoC(unittest.TestCase):
 
     def test_part(self):
-        assert_with_message([[59, 1], [4, 2]], find_highest_and_penultimate([[2, 0], [59, 1], [4, 2], [3, 3]]))
-        assert_with_message([[11, 2], [4, 1]], find_highest_and_penultimate([[2, 0], [4, 1], [11, 2], [3, 3]]))
-        assert_with_message([[100, 0], [33, 3]], find_highest_and_penultimate([[100, 0], [4, 1], [11, 2], [33, 3]]))
-        assert_with_message([[100, 3], [33, 0]], find_highest_and_penultimate([[33, 0], [4, 1], [11, 2], [100, 3]]))
-        assert_with_message([[2, 0], [2, 2]], find_highest_and_penultimate([[2, 0], [2, 2], [2, 4]]))
-        assert_with_message(11, get_consecutive_schedule_with_minimum([11, 0], [4, 1], 10))
-        assert_with_message(15, get_consecutive_schedule_with_minimum([5, 0], [4, 1], 10))
-        assert_with_message(8, get_consecutive_schedule_with_minimum([2, 0], [5, 2], 0))
-        assert_with_message(18, get_consecutive_schedule_with_minimum([2, 0], [5, 2], 10))
+        assert_with_message([11, 12], get_consecutive_schedule_with_minimum([11, 0], [4, 1], [10, 10]))
+        assert_with_message([15, 16], get_consecutive_schedule_with_minimum([5, 0], [4, 1], [10, 10]))
+        assert_with_message([8, 10], get_consecutive_schedule_with_minimum([2, 0], [5, 2], [0, 0]))
+        assert_with_message([18, 20], get_consecutive_schedule_with_minimum([2, 0], [5, 2], [10, 10]))
 
         given_input1 = ['939', '7,13,x,x,59,x,31,19']
         processed_given_input1 = process_input(given_input1)
