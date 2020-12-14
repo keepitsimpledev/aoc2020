5a722ebd710e63f67c46107b1cb71c2709d648d5
dev <keep.it.simple.dev@gmail.com>
2020-12-13 21:51:40 -0500

    day13 part2 potentially improved, but still not fast enough

--- day13/part2.py
+++ day13/part2.py
@@ -1,5 +1,6 @@
 import unittest
 
+
 from aoc_util import assert_with_message
 from aoc_util import get_input
 
@@ -13,27 +14,76 @@ def process_input(time_and_schedules):
     return schedules
 
 
-def find_consecutive_schedule(bus_and_interval):
-    values = []
-    for bus in bus_and_interval:
-        values += [bus[0]]
-    current_index = 0
-    path_found = False
-    while not path_found:
-        curr_val = values[current_index]
-        next_val = bus_and_interval[current_index + 1][0]
-        next_val = int(curr_val / next_val) * next_val + next_val
-        values[current_index + 1] = next_val
-        gap = bus_and_interval[current_index + 1][1] - bus_and_interval[current_index][1]
-        if curr_val == next_val - gap:
-            if current_index == len(bus_and_interval) - 2:
-                path_found = True
+def find_consecutive_schedule(busses_and_intervals):
+    current_values = []
+    for item in busses_and_intervals:
+        current_values += [item[0]]
+    highest_and_second = find_highest_and_penultimate(busses_and_intervals)
+    for i in range(len(busses_and_intervals)):
+        if busses_and_intervals[i][0] == highest_and_second[0][0]:
+            fulcrum_index = i
+            break
+    current_values[fulcrum_index] = get_consecutive_schedule_with_minimum(highest_and_second[0],
+                                                                          highest_and_second[1], 0)
+    solution_found = False
+    while not solution_found:
+        current_solution_looks_good = True
+        left_index = fulcrum_index
+        while left_index > 0:
+            current_value = current_values[left_index]
+            left_bus = busses_and_intervals[left_index - 1][0]
+            left_gap = busses_and_intervals[left_index][1] - busses_and_intervals[left_index - 1][1]
+            if (current_value - left_gap) % left_bus == 0:
+                current_values[left_index - 1] = current_value - left_gap
+                left_index -= 1
             else:
-                current_index += 1
+                current_solution_looks_good = False
+                break
+        if current_solution_looks_good:
+            right_index = fulcrum_index
+            while right_index < len(busses_and_intervals) - 1:
+                current_value = current_values[right_index]
+                right_bus = busses_and_intervals[right_index + 1][0]
+                right_gap = busses_and_intervals[right_index + 1][1] - busses_and_intervals[right_index][1]
+                if (current_value + right_gap) % right_bus == 0:
+                    current_values[right_index + 1] = current_value + right_gap
+                    right_index += 1
+                else:
+                    current_solution_looks_good = False
+                    break
+        if current_solution_looks_good:
+            return current_values[0]
         else:
-            values[0] += bus_and_interval[0][0]
-            current_index = 0
-    return values[0]
+            current_values[fulcrum_index] = get_consecutive_schedule_with_minimum(
+                highest_and_second[0], highest_and_second[1], current_values[fulcrum_index] + 1)
+
+
+def find_highest_and_penultimate(bus_and_interval):
+    highest = [-1, -1]
+    second_highest = [-1, -1]
+    for item in bus_and_interval:
+        if item[0] > highest[0]:
+            second_highest = highest
+            highest = item
+        elif second_highest[0] < item[0] <= highest[0]:
+            second_highest = item
+    return [highest, second_highest]
+
+
+def get_consecutive_schedule_with_minimum(bus1, bus2, minimum):
+    bus1_time = int(minimum / bus1[0]) * bus1[0]
+    if bus1_time < minimum or bus1_time == 0:
+        bus1_time += bus1[0]
+    bus2_time = int(minimum / bus2[0]) * bus2[0]
+    if bus2_time < minimum or bus2_time == 0:
+        bus2_time += bus2[0]
+    gap = bus2[1] - bus1[1]
+    while bus1_time != bus2_time - gap:
+        if bus2_time - bus1_time >= gap:
+            bus1_time += bus1[0]
+        else:
+            bus2_time += bus2[0]
+    return bus1_time
 
 
 def run():
@@ -43,6 +93,16 @@ def run():
 class TestAoC(unittest.TestCase):
 
     def test_part(self):
+        assert_with_message([[59, 1], [4, 2]], find_highest_and_penultimate([[2, 0], [59, 1], [4, 2], [3, 3]]))
+        assert_with_message([[11, 2], [4, 1]], find_highest_and_penultimate([[2, 0], [4, 1], [11, 2], [3, 3]]))
+        assert_with_message([[100, 0], [33, 3]], find_highest_and_penultimate([[100, 0], [4, 1], [11, 2], [33, 3]]))
+        assert_with_message([[100, 3], [33, 0]], find_highest_and_penultimate([[33, 0], [4, 1], [11, 2], [100, 3]]))
+        assert_with_message([[2, 0], [2, 2]], find_highest_and_penultimate([[2, 0], [2, 2], [2, 4]]))
+        assert_with_message(11, get_consecutive_schedule_with_minimum([11, 0], [4, 1], 10))
+        assert_with_message(15, get_consecutive_schedule_with_minimum([5, 0], [4, 1], 10))
+        assert_with_message(8, get_consecutive_schedule_with_minimum([2, 0], [5, 2], 0))
+        assert_with_message(18, get_consecutive_schedule_with_minimum([2, 0], [5, 2], 10))
+
         given_input1 = ['939', '7,13,x,x,59,x,31,19']
         processed_given_input1 = process_input(given_input1)
         assert_with_message([[7, 0], [13, 1], [59, 4], [31, 6], [19, 7]], processed_given_input1)
@@ -72,6 +132,6 @@ class TestAoC(unittest.TestCase):
         assert_with_message(1202161486, find_consecutive_schedule(test_input5))
 
         day_input = process_input(get_input(13))
-        # assert_with_message(1202161486, find_consecutive_schedule(day_input))
+        assert_with_message(1202161486, find_consecutive_schedule(day_input))
 
         run()
